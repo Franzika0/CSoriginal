c0e38e362d159f13d34312c9498b7d12d382116e
Franzika0 <ForHS05.08@gmail.com>
2022-10-19 04:45:23 +0800

    'other-file'

--- PWA.js
+++ PWA.js
@@ -2,6 +2,7 @@ function init(){
 
     $.noConflict;
     jQuery(document).ready(($)=>{
+
         
         $('#t').click(()=>{
             $('#t').animate({
@@ -10,25 +11,45 @@ function init(){
             },1000)
         });
 
-        function MakeMainScene(){
+        function MakeMainScene(screen_width){
             $('#device').append("<div mid id='FightPlace'></div>");
-            calculateFightPlace();
-            $('#FightPlace').append("<div mid id='Road_Outer'></div>");
-            $('#FightPlace').append("<div mid id='Road_Inner'></div>");
-            $('#Road_Inner').append("<div smallcastle id='Left_Player_SmallCastle'></div>");
-            $('#Road_Inner').append("<div smallcastle id='Right_Player_SmallCastle'></div>");
-            $('#Road_Inner').append("<div smallcastle id='Left_Enemy_SmallCastle'></div>");
-            $('#Road_Inner').append("<div smallcastle id='Right_Enemy_SmallCastle'></div>");
-            $('#Road_Inner').append("<div maincastle id='Enemy_MainCastle'></div>");
-            $('#Road_Inner').append("<div maincastle id='Player_MainCastle'></div>");
+            $('#FightPlace').css({width : `${screen_width*0.3}px`,height: `${screen_width*0.48}px`});
+            $('#FightPlace').append("<div mid id='Road_Outer'></div>"
+                                   +"<div mid id='Road_Inner'></div>");
+            $('#Road_Inner').append("<div smallcastle id='Left_Player_SmallCastle'></div>"
+                                   +"<div smallcastle id='Right_Player_SmallCastle'></div>"
+                                   +"<div smallcastle id='Left_Enemy_SmallCastle'></div>"
+                                   +"<div smallcastle id='Right_Enemy_SmallCastle'></div>"
+                                   +"<div maincastle id='Enemy_MainCastle'></div>"
+                                   +"<div maincastle id='Player_MainCastle'></div>"
+                                   +"<div mid id='River'></div>"
+                                   +"<div mid bridge id='Left_Bridge'></div>"
+                                   +"<div mid bridge id='Right_Bridge'></div>");
         }
 
-        function calculateFightPlace(){
-            let width = window.innerWidth;
-            $('#FightPlace').css({width : `${width*0.3}px`,height: `${width*0.48}px`});
+
+        MakeMainScene(window.innerWidth);
+
+
+        class Scene_MainPoints{
+            //player_Left_Castle_point = [$('#Left_Player_SmallCastle').position().left,$('#Left_Player_SmallCastle').position().top];
+            player_Left_Castle_point = [0,0];
+            player_Right_Castle_point = $('#Right_Player_SmallCastle').position().right;
+
+            MakePoints(){
+                /*$('#FightPlace').append("<div mid point style='left:"+(this.player_Left_Castle_point[0])+"px;top:"
+                +(this.player_Left_Castle_point[1])+"px'></div>");*/
+                $('#FightPlace').append("<div mid point></div>");
+            }
         }
 
-        MakeMainScene();
+        const Scene_MainPoints_obj = new Scene_MainPoints();
+        Scene_MainPoints_obj.MakePoints();
+
+        //$('#Player_MainCastle').text(Scene_MainPoints_obj.player_Left_Castle_point);
+        /*$('#Player_MainCastle').click(()=>{
+            $('#Player_MainCastle').text(Scene_MainPoints_obj.player_Left_Castle_point);
+        });*/
 
 
 
